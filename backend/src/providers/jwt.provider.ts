1d035176e408462b693e3a22eac576584fc4d3f5
Matheus <stvgamer74@gmail.com>
2023-01-17 13:30:15 -0300

    feat: :sparkles: create bcrypt and jwt providers

---
+++ backend/src/providers/jwt.provider.ts
@@ -0,0 +1,34 @@
+import * as dotenv from 'dotenv';
+import { verify, sign, SignOptions, JwtPayload } from 'jsonwebtoken';
+import { ErrorTypes } from '../errors/catalog';
+
+dotenv.config();
+
+const SECRET = `${process.env.JWT_SECRET}`;
+const options: SignOptions = { expiresIn: '15d', algorithm: 'HS256' };
+
+export interface IUserJWT {
+  userName: string,
+}
+
+class JwtProvider {
+  constructor(private secret: string, private signOptions: SignOptions) {}
+
+  signUser(user: IUserJWT): string {
+    return this.sign({ user });
+  }
+
+  sign(payload: JwtPayload): string {
+    return sign(payload, this.secret, this.signOptions);
+  }
+
+  verify(token: string): JwtPayload {
+    try {
+      return verify(token, this.secret, this.signOptions) as JwtPayload;
+    } catch (_err) {
+      throw Error(ErrorTypes.InvalidCredentials);
+    }
+  }
+}
+
+export default new JwtProvider(SECRET, options);
