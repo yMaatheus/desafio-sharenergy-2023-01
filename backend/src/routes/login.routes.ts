c33c93a2fabce781e7b281278d2d4d2f777ca411
Matheus <stvgamer74@gmail.com>
2023-01-20 14:12:35 -0300

    refactor: :recycle: change route /user/me to /login/me

--- backend/src/routes/login.routes.ts
+++ backend/src/routes/login.routes.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express';
+import { StatusCodes } from 'http-status-codes';
 import { User } from '../models/User';
 import UserService from '../services/user.service';
 import UserController from '../controllers/user.controller';
+import authMiddleware from '../middlewares/auth.middleware';
 
 const userODM = new User();
 const userService = new UserService(userODM);
@@ -11,5 +13,9 @@ const route = Router();
 
 route.route('/')
   .post(userController.login);
+
+route.get('/me', authMiddleware, (_req, res) => (
+  res.status(StatusCodes.OK).json(res.locals.user)
+));
 
 export default route;
