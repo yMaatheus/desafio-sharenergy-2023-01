6f5ee81563d851f2416e4026317a62cc2a713e5c
Matheus <stvgamer74@gmail.com>
2023-01-21 00:26:34 -0300

    fix: :bug: fix input search http code in cat page

--- frontend/src/pages/CatPage/CatPage.tsx
+++ frontend/src/pages/CatPage/CatPage.tsx
@@ -1,28 +1,31 @@
 import { useState } from "react";
+import InputMask from "react-input-mask";
 
 const CatPage = () => {
+  const [code, setCode] = useState<number>(200);
   const [httpCode, setHttpCode] = useState<number>(200);
+  const [error, setError] = useState<string>("");
   const url = `https://http.cat/${httpCode}.jpg`;
 
-  const handleHttpCode = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const code = +event.target.value;
+  const handleHttpCode = () => {
     if (code < 100 || code > 599) {
+      setError("Número inválido.");
       return;
     }
-    setHttpCode(+event.target.value);
+    setHttpCode(code);
   };
+
+  const handleText = (event: React.ChangeEvent<HTMLInputElement>) =>
+    setCode(+event.target.value);
 
   return (
     <div>
       <h1>{httpCode}</h1>
-      <input
-        type="number"
-        name="code"
-        min="100"
-        max="599"
-        value={httpCode}
-        onChange={handleHttpCode}
-      />
+      <div>{error && <span>Error: {error}</span>}</div>
+      <input type="text" value={code} onChange={handleText} />
+      <button type="button" onClick={handleHttpCode}>
+        Buscar
+      </button>
       <section>
         <img src={url} alt="Imagem HttpCat" />
       </section>
